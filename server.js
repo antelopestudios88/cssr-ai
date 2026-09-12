cdbeb5974d91eba76a492710c122b4eceb4122c0
antelopestudios88 <your-new-email@gmail.com>
2026-09-12 22:06:12 +0300

    Update server.js and index.html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Allow JSON requests
 app.use(express.json());
